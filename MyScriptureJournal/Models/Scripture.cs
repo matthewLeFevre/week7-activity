afa4906d35da74d1aec27352e3933cbdf4527ca6
Matthew LeFevre <matthewlefevre95@gmail.com>
2019-06-06 11:17:39 -0600

    Add project files.

---
+++ MyScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ID { get; set; }
+
+        [Display(Name = "Date Added")]
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+
+        [StringLength(15, MinimumLength = 3)]
+        [Required]
+        public String Book {get; set;}
+
+        [Required]
+        public String Reference { get; set; }
+
+        public String Notes { get; set; }
+
+
+    }
+}
---
+++ MyScriptureJournal/Pages/Scriptures/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyScriptureJournal.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MyScriptureJournal.Pages.Scriptures
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyScriptureJournal.Models.MyScriptureJournalContext _context;
+
+        public IndexModel(MyScriptureJournal.Models.MyScriptureJournalContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Scripture> Scripture { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Book { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string ScriptureBook { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var scriptures = from s in _context.Scripture
+                         select s;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                scriptures = scriptures.Where(s => s.Notes.Contains(SearchString));
+            }
+            if (!string.IsNullOrEmpty(ScriptureBook))
+            {
+                scriptures = scriptures.Where(x => x.Book == ScriptureBook);
+            }
+
+            Scripture = await scriptures.ToListAsync();
+        }
+    }
+}
